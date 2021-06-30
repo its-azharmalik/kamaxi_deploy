75af8719bb49c39df8bde210d0fafab6dacb5aeb
its-azharmalik <azharmahmood6338@gmail.com>
2021-06-30 12:36:16 +0530

    lfs reset

---
+++ js/form.js
@@ -0,0 +1,24 @@
+document.querySelector('.wpcf7').onsubmit = (e) =>{
+    e.preventDefault();
+    var urls = [
+    '/ayan.pdf',
+    '/azhar.pdf'
+    ]
+
+    var interval = setInterval(download, 300, urls);
+
+    function download(urls) {
+        var url = urls.pop();
+
+        var a = document.createElement("a");
+        a.setAttribute('href', url);
+        a.setAttribute('download', '');
+        a.setAttribute('target', '_blank');
+        a.click();
+
+        if (urls.length == 0) {
+            clearInterval(interval);
+        }
+    }
+
+    }
