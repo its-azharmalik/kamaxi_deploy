d07b9fc92283e842cb2192104e2f20e39cfd6325
its-azharmalik <azharmahmood6338@gmail.com>
2021-07-01 14:04:50 +0530

    final

--- js/form.js
+++ js/form.js
@@ -23,3 +23,29 @@ document.querySelector('.wpcf7').onsubmit = (e) =>{
     }
 
     }
+
+document.querySelector('.mc4wp-form').onsubmit = (e) =>{
+        e.preventDefault();
+        var urls = [
+        '/kamaxiHeights.pdf',
+        '/kamaxiGreen.pdf',
+        '/kamaxiKunj.pdf'
+        ]
+    
+        var interval = setInterval(download, 300, urls);
+    
+        function download(urls) {
+            var url = urls.pop();
+    
+            var a = document.createElement("a");
+            a.setAttribute('href', url);
+            a.setAttribute('download', '');
+            a.setAttribute('target', '_blank');
+            a.click();
+    
+            if (urls.length == 0) {
+                clearInterval(interval);
+            }
+        }
+    
+        }
