6939b519441e069b65bcf388b0b775f346cd73c3
svnitad <svnitad@gmail.com>
2022-04-05 00:52:00 +0530

    edits form submission

--- js/form.js
+++ js/form.js
@@ -19,27 +19,6 @@
 //   }
 // };
 
-document.querySelector(".allPdfs").onsubmit = (e) => {
-  e.preventDefault();
-  var urls = ["/kamaxiHeights.pdf", "/kamaxiGreen.pdf", "/kamaxiKunj.pdf"];
-
-  var interval = setInterval(download, 300, urls);
-
-  function download(urls) {
-    var url = urls.pop();
-
-    var a = document.createElement("a");
-    a.setAttribute("href", url);
-    a.setAttribute("download", "");
-    a.setAttribute("target", "_blank");
-    a.click();
-
-    if (urls.length == 0) {
-      clearInterval(interval);
-    }
-  }
-};
-
 // document.querySelector(".heightPdfs").onsubmit = (e) => {
 //   e.preventDefault();
 //   var urls = ["/kamaxiHeights.pdf"];
@@ -62,38 +41,58 @@ document.querySelector(".allPdfs").onsubmit = (e) => {
 //   }
 // };
 
-document.querySelector(".greenPdfs").onsubmit = (e) => {
-  e.preventDefault();
-  var urls = ["/kamaxiGreen.pdf"];
+const locationURL = window.location.href.split("/");
+const loclength = locationURL.length;
 
-  var interval = setInterval(download, 300, urls);
-
-  function download(urls) {
-    var url = urls.pop();
-
+if (locationURL[loclength - 1] == "green.html") {
+  document.querySelectorAll(".greenPdfs").onsubmit = (e) => {
+    e.preventDefault();
+    var url = "./kamaxiGreen.pdf";
     var a = document.createElement("a");
-    a.setAttribute("href", url);
+    a.href = url;
     a.setAttribute("download", "");
-    a.setAttribute("target", "_blank");
-    a.click();
+    a.download = "kamaxiGreen.pdf";
+    a.dispatchEvent(new MouseEvent("click"));
+  };
+} else if (locationURL[loclength - 1] == "heights.html") {
+  document.querySelectorAll(".heightPdfs").onsubmit = (e) => {
+    e.preventDefault();
+    var url = "./kamaxiHeights.pdf";
+    var a = document.createElement("a");
+    a.href = url;
+    a.setAttribute("download", "");
+    a.download = "kamaxiHeights.pdf";
+    a.dispatchEvent(new MouseEvent("click"));
+  };
+} else if (locationURL[loclength - 1] == "kunj.html") {
+  document.querySelectorAll(".kunjPdfs").onsubmit = (e) => {
+    e.preventDefault();
+    var url = "./kamaxiKunj.pdf";
+    var a = document.createElement("a");
+    a.href = url;
+    a.setAttribute("download", "");
+    a.download = "kamaxiGreen.pdf";
+    a.dispatchEvent(new MouseEvent("click"));
+  };
+} else if (locationURL[loclength - 1] == "") {
+  document.querySelectorAll(".allPdfs").onsubmit = (e) => {
+    e.preventDefault();
+    var urls = ["/kamaxiHeights.pdf", "/kamaxiGreen.pdf", "/kamaxiKunj.pdf"];
 
-    if (urls.length == 0) {
-      clearInterval(interval);
+    var interval = setInterval(download, 300, urls);
+
+    function download(urls) {
+      var url = urls.pop();
+
+      var a = document.createElement("a");
+      a.setAttribute("href", url);
+      a.setAttribute("download", "");
+      a.setAttribute("target", "_blank");
+      a.click();
+
+      if (urls.length == 0) {
+        clearInterval(interval);
+      }
     }
-  }
-};
-
-function download(url) {
-  var a = document.createElement("a");
-  a.setAttribute("href", url);
-  a.setAttribute("download", "");
-  a.setAttribute("target", "_blank");
-  a.click();
+  };
 }
-
-document.querySelector(".heightPdfs").onsubmit = (e) => {
-  e.preventDefault();
-
-  var url = "/kamaxiKunj.pdf";
-  download(url);
-};
